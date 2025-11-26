8446983d3bd5ce281893b073ddb0c93ede705694
LucasValpereiro <lucasvalpereiro@hotmail.com>
2025-11-26 14:06:33 -0300

    added images and order to the pokemons

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,6 @@
 
 function convertPokemonTypesToLi(pokemonTypes) {
-  return pokemonTypes.map((type) => `<li class="type">${type}</li>`)
+  return pokemonTypes.map((typeSlot) => `<li class="type">${typeSlot.type.name}</li>`)
 }
 
 function convertPokemonToLi(pokemon) {
@@ -10,11 +10,10 @@ function convertPokemonToLi(pokemon) {
 
           <div class="detail">
             <ol class="types">
-                <li class="type">Grass</li>
-                <li class="type">Poison</li>
+                ${convertPokemonTypesToLi(pokemon.types).join('')}
             </ol>
           <img
-          src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/1.svg"
+          src="${pokemon.sprites.other.dream_world.front_default}"
           alt="${pokemon.name}">
           </div>
         </li>
--- assets/js/poke_api.js
+++ assets/js/poke_api.js
@@ -4,7 +4,7 @@ pokeApi.getPokemonsDetail = (pokemon) => {
     return fetch(pokemon.url).then((response) => response.json())
 }
 
-pokeApi.getPokemons = (offset = 0, limit = 10) => {
+pokeApi.getPokemons = (offset = 0, limit = 5) => {
   const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`
 
    return fetch(url)
