efffa9003ddf2813b693da1b99efdac14f8b5f70
LucasValpereiro <lucasvalpereiro@hotmail.com>
2025-11-27 11:54:46 -0300

    added limit to remove button

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,8 +2,8 @@ const pokemonList = document.getElementById("pokemonList");
 const loadMoreButton = document.getElementById("loadMoreButton");
 
 
-const maxRecords = 11;
-const limit = 5;
+const maxRecords = 151;
+const limit = 10;
 let offset = 0;
 
 function loadPokemonItems(offset, limit) {
