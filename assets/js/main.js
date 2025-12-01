4892822f8f0bcc9c721c9de6d1da19c4e9b482ff
LucasValpereiro <lucasvalpereiro@hotmail.com>
2025-12-01 15:52:41 -0300

    added new functions to seach pokemons

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,15 +1,16 @@
 const pokemonList = document.getElementById("pokemonList");
 const loadMoreButton = document.getElementById("loadMoreButton");
+const searchInput = document.getElementById("searchInput"); // Nova const para buscar o ID
 
 
 const maxRecords = 151;
 const limit = 10;
 let offset = 0;
+let allPokemons = []; // Armazena os pokemons carregados
 
-function loadPokemonItems(offset, limit) {
-  pokeApi.getPokemons(offset, limit).then((pokemons = []) => {
-    const newHtml = pokemons.map((pokemon) => `
-        <li class="pokemon ${pokemon.type}"> 
+// Função nova para dar retorno e não repetir
+function convertPokemonToLi(pokemon) {
+  return ` <li class="pokemon ${pokemon.type}"> 
           <span class="number">${pokemon.number}</span>
           <span class="name">${pokemon.name}</span>
 
@@ -24,15 +25,36 @@ function loadPokemonItems(offset, limit) {
           alt="${pokemon.name}">
           </div>
         </li>
-        `).join('');
-
-    pokemonList.innerHTML += newHtml
-  })
+        `
 }
+
+function loadPokemonItems(offset, limit) {
+  pokeApi.getPokemons(offset, limit).then((pokemons = []) => {
+    allPokemons = allPokemons.concat(pokemons); // Ir adicionando a busca dos pokemons conforme são exibidos mais
+    const newHtml = pokemons.map(convertPokemonToLi).join('');
+    pokemonList.innerHTML += newHtml
+});
+}
+
+// Novo filtro para exibir o Pokemon
+function filterPokemons(searchTerm) {
+  const filtered = allPokemons.filter((pokemon) =>
+  pokemon.name.toLowerCase().includes(searchTerm.toLowerCase())
+);
+pokemonList.innerHTML = filtered.map(convertPokemonToLi).join('');
+}
+
+// Evento para busca
+searchInput.addEventListener('input', (event) => {
+  const searchTerm = event.target.value;
+  filterPokemons(searchTerm);
+});
 
 loadPokemonItems(offset, limit)
 
 loadMoreButton.addEventListener('click', () => {
+  searchInput.value = '';
+
   offset += limit
 
   const qtdRecordsWithNextPage = offset + limit
